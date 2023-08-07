ef4bd1254d4590739286a42aa019c75edb94872c
Iman Kamrava <73425274+Ikamrava@users.noreply.github.com>
2023-08-07 11:31:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export const getServerSideProps = async () => {
 
   try {
 
-    const featuredId =  "6474cd6e46b89b5d43bc7318"
-    const query = {};
-    const options = { sort: { _id: 1 } };
+    const featuredId =  "64d0c3ca7d894e14f3cffe3a"
+
     const client = await clientPromise;
        const db = client.db("test");
 
@@ -27,7 +26,7 @@ export const getServerSideProps = async () => {
       props: { 
         isConnected: true,
         products: JSON.parse(JSON.stringify(products)),
-        product: JSON.parse(JSON.stringify(products.findOne(query, options)))
+        product: JSON.parse(JSON.stringify(products.find(product => product._id == featuredId)))
 
        },
     }
